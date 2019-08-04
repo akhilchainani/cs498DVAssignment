86c2815c987c0c45f3b2b49f34571f972f018d21
akhilchainani <adc2@illinois.edu>
2019-08-04 15:30:08 -0400

    Update cityBarChart.js

--- cityBarChart.js
+++ cityBarChart.js
@@ -1,5 +1,49 @@
 var cityBarChart={
   display: function () {
-        
+        var margin = {
+                top: 20,
+                right: 20,
+                bottom: 30,
+                left: 100
+            };
+        var width = 960 - margin.left - margin.right;
+        var height = 600 - margin.top - margin.bottom;
+    
+        const data = d3.csv("https://raw.githubusercontent.com/akhilchainani/cs498DVAssignment/master/Preprocessed_Rent.csv"),
+                            function(d) {
+                              return {
+                                Location: d.Location,
+                                Location_Type: d.Location_Type,
+                                State: d.State,
+                                Bedroom_Size: d.Bedroom_Size,
+                                Price_2014_AVG: +d.Price_2014_AVG,
+                                Price_2015_AVG: +d.Price_2015_AVG,
+                                Price_2016_AVG: +d.Price_2016_AVG,
+                                Price_2017_AVG: +d.Price_2017_AVG,
+                                Price_2018_AVG: +d.Price_2018_AVG,
+                                Price_2019_AVG: +d.Price_2019_AVG
+                              };
+                            });
+        var studioData = data.filter(function(d) {
+                                      return d.Bedroom_Side === "Studio";
+                                     });
+        var OneBrData = data.filter(function(d) {
+                                      return d.Bedroom_Side === "1br";
+                                    });
+        var TwoBrData = data.filter(function(d) {
+                                      return d.Bedroom_Side === "2br";
+                                    });
+        var ThreeBrData = data.filter(function(d) {
+                                      return d.Bedroom_Side === "3br";
+                                      });
+        var FourBrData = data.filter(function(d) {
+                                      return d.Bedroom_Side === "4br";
+                                     });
+        d3.select('body')
+        .selectAll('p')
+        .data(studioData)
+        .enter()
+        .append('p')
+        .html(function(d,i) { return d.Location + " " + d.Price_2019_AVG; });
   }
 };
